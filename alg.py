af6225d4961da94553c4eb8a174e4ca9b9875d6a
XXXzp <821030921@qq.com>
2017-09-04 22:22:57 +0800

    change the args for debug

--- alg.py
+++ alg.py
@@ -4,13 +4,12 @@ import sys
 import time
 
 def main():
-    port = 18791
-    playerName = 'Alice'
-    #port = int(sys.argv[1])
-    #seatNum = int(sys.argv[2])
-    #playerName = sys.argv[3]
-    logpath = '/home/goodman/POKER/project_acpc_server_v1.0.41/project_acpc_server/matchtry.log'
-    p1 = player.Player(playerName,port,logpath)
+    port = int(sys.argv[1])
+    logpath = sys.argv[2]
+    playerName = sys.argv[3]
+
+
+    ply = player.Player(playerName,port,logpath)
     f = open('log.txt','w')
     Total_reward = 0.0
     error = 0
@@ -20,13 +19,13 @@ def main():
     # 不断接受dealer的信息，判别是不是自己的动作时机，如果是自己的动作时机就生成动作
     # 这里不考虑动作是否合法
     while episode<1001:
-        msgQueue = p1.recvMsg()
+        msgQueue = ply.recvMsg()
         for m in msgQueue:
             msg = m
-            flag = p1.handleMsg(msg)
+            flag = ply.handleMsg(msg)
             if flag == 0:#act
                 action = random.randint(0, 2)
-                p1.step(msg, action)
+                ply.step(msg, action)
                 continue
             if flag ==9999999:#clusy way of show flag
                 continue
